4f5d0ba482470249737f67f97d394cc5033a4ba4
marcinn <01171345@pw.edu.pl>
2023-12-26 23:02:40 +0100

    Add project files.

---
+++ cmakeOpenGL-Template/cmakeOpenGL-Template.cpp
@@ -0,0 +1,12 @@
+﻿// cmakeOpenGL-Template.cpp : Defines the entry point for the application.
+//
+
+#include "cmakeOpenGL-Template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
