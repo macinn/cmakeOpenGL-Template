70cba9f15f6c42d826527788b5c8f9f246b10bc4
marcinn <01171345@pw.edu.pl>
2023-12-26 23:50:24 +0100

    fix glew

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,47 @@
-#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main(int argc, const char* argv[]) 
+int main(void)
 {
-	std::cout << "Hello world!!!";
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        return -1;
+    }
+
+    unsigned int a;
+    glGenBuffers(1, &a);
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
